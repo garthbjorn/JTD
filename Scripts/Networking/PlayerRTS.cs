bd9c607503750a0ed037a2ebede4caec2a29a118
garth <garthcline@gmail.com>
2021-08-05 00:40:25 -0400

    add

---
+++ Scripts/Networking/PlayerRTS.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class PlayerRTS : NetworkBehaviour
+{
+    [SerializeField] private Building[] buildings = new Building[0];
+
+    [SyncVar(hook = nameof(ClientHandleResourcesUpdated))]
+    private int resources = 500;
+
+    public event Action<int> ClientOnResourcesUpdated;
+
+    private List<Unit> myUnits = new List<Unit>();
+    private List<Building> myBuildings = new List<Building>();
+
+    public List<Unit> GetMyUnits()
+    {
+        return myUnits;
+    }
+
+    public List<Building> GetMyBuildings()
+    {
+        return myBuildings;
+    }   
+
+    public int GetResources()
+    {
+        return resources;
+    }
+
+    [Server]
+    public void SetResources(int newResources)
+    {
+        resources = newResources;
+    }
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+        Building.ServerOnBuildingSpawned += ServerHandleBuildingSpawned;
+        Building.ServerOnBuildingDespawned += ServerHandleBuildingDespawned;
+
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+        Building.ServerOnBuildingSpawned -= ServerHandleBuildingSpawned;
+        Building.ServerOnBuildingDespawned -= ServerHandleBuildingDespawned;
+    }
+    
+    [Command]
+    public void CmdTryPlaceBuilding(int buildingID, Vector3 point)
+    {
+        Building buildingToPlace = null;
+        foreach(Building building in buildings)
+        {
+            if(building.GetId() == buildingID)
+            {
+                buildingToPlace = building;
+                break;
+            }
+        }
+
+        if(buildingToPlace == null) {return;}
+
+        GameObject buildingInstance = 
+            Instantiate(buildingToPlace.gameObject, point,buildingToPlace.transform.rotation);
+
+        NetworkServer.Spawn(buildingInstance, connectionToClient);
+    }
+
+    private void ServerHandleUnitSpawned(Unit unit)
+    {
+        if(unit.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+        myUnits.Add(unit);
+    }
+
+    private void ServerHandleUnitDespawned(Unit unit)
+    {
+        if(unit.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+        myUnits.Remove(unit);
+    }
+
+    private void ServerHandleBuildingSpawned(Building building)
+    {
+        if(building.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+        myBuildings.Add(building);
+    }
+
+    private void ServerHandleBuildingDespawned(Building building)
+    {
+        if(building.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+        myBuildings.Remove(building);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        if(NetworkServer.active) {return;}
+        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+        Building.AuthorityOnBuildingSpawned += AuthorityHandleBuildingSpawned;
+        Building.AuthorityOnBuildingDespawned += AuthorityHandleBuildingDespawned;
+    }
+
+    public override void OnStopClient()
+    {
+        if(!isClientOnly || !hasAuthority) {return;}
+        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+        Building.AuthorityOnBuildingSpawned -= AuthorityHandleBuildingSpawned;
+        Building.AuthorityOnBuildingDespawned -= AuthorityHandleBuildingDespawned;
+        
+    }
+
+    private void ClientHandleResourcesUpdated(int oldVal, int newVal)
+    {
+        ClientOnResourcesUpdated?.Invoke(newVal);
+    }
+
+    private void AuthorityHandleUnitSpawned(Unit unit)
+    {
+        myUnits.Add(unit);
+    }
+
+    private void AuthorityHandleUnitDespawned(Unit unit)
+    {
+        myUnits.Remove(unit);
+    }
+    
+    private void AuthorityHandleBuildingSpawned(Building building)
+    {
+        myBuildings.Add(building);
+    }
+
+    private void AuthorityHandleBuildingDespawned(Building building)
+    {
+        myBuildings.Remove(building);
+    }
+
+    #endregion
+}
---
+++ Scripts/Units/UnitCommandGiver.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitCommandGiver : MonoBehaviour
+{
+    [SerializeField] private UnitSelectionHandler unitSelectionHandler = null;
+    [SerializeField] private LayerMask layerMask = new LayerMask();
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        GameOverHandler.ClientOnGameOver += ClientHandleGameOver;
+        
+    }
+
+    private void OnDestroy()
+    {
+        GameOverHandler.ClientOnGameOver -= ClientHandleGameOver;
+
+    }
+
+    private void Update()
+    {
+        if(!Mouse.current.rightButton.wasPressedThisFrame) {return;}
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) {return;}
+
+        if(hit.collider.TryGetComponent<Targetable>(out Targetable target))
+        {
+            if(target.hasAuthority)
+            {
+                
+                // Target is my own unit
+                TryMove(hit.point);
+                return;
+            }
+
+            // Target is enemy unit
+            TryTarget(target);
+            return;
+        }
+
+        // Target is just a position
+        TryMove(hit.point);
+    }
+
+    private void TryMove(Vector3 point)
+    {
+        foreach(Unit unit in unitSelectionHandler.SelectedUnits)
+        {
+            unit.GetUnitMovement().CmdMove(point);
+        }
+    }
+
+    private void TryTarget(Targetable target)
+    {
+        foreach(Unit unit in unitSelectionHandler.SelectedUnits)
+        {
+            unit.GetTargeter().CmdSetTarget(target.gameObject);
+        }
+    }
+
+    private void ClientHandleGameOver(string winner)
+    {
+        enabled = false;
+    }
+}
