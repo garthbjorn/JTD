442de834d6623ec29b6144e80127a7d2e2734990
Garth <garthcline@gmail.com>
2021-12-27 15:30:12 -0500

    Add Plan and clear example code

--- Assets/Scripts/Networking/NetworkManagerRTS.cs
+++ Assets/Scripts/Networking/NetworkManagerRTS.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class NetworkManagerRTS : NetworkManager
 {
     //[SerializeField] private GameObject unitSpawnerPrefab = null;
-    [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
+    // [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
@@ -25,9 +25,9 @@ public class NetworkManagerRTS : NetworkManager
     {
         if(SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
         {
-            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+            // GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
 
-            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+            // NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
 
         }
     }
--- Assets/Scripts/Networking/PlayerTD.cs
+++ Assets/Scripts/Networking/PlayerTD.cs
@@ -5,148 +5,148 @@ using UnityEngine;
 
 public class PlayerTD : NetworkBehaviour
 {
-    [SerializeField] private Building[] buildings = new Building[0];
+    // [SerializeField] private Building[] buildings = new Building[0];
 
-    [SyncVar(hook = nameof(ClientHandleResourcesUpdated))]
-    private int resources = 500;
+    // [SyncVar(hook = nameof(ClientHandleResourcesUpdated))]
+    // private int resources = 500;
 
-    public event Action<int> ClientOnResourcesUpdated;
+    // public event Action<int> ClientOnResourcesUpdated;
 
-    private List<Unit> myUnits = new List<Unit>();
-    private List<Building> myBuildings = new List<Building>();
+    // private List<Unit> myUnits = new List<Unit>();
+    // private List<Building> myBuildings = new List<Building>();
 
-    public List<Unit> GetMyUnits()
-    {
-        return myUnits;
-    }
+    // public List<Unit> GetMyUnits()
+    // {
+    //     return myUnits;
+    // }
 
-    public List<Building> GetMyBuildings()
-    {
-        return myBuildings;
-    }   
+    // public List<Building> GetMyBuildings()
+    // {
+    //     return myBuildings;
+    // }   
 
-    public int GetResources()
-    {
-        return resources;
-    }
+    // public int GetResources()
+    // {
+    //     return resources;
+    // }
 
-    [Server]
-    public void SetResources(int newResources)
-    {
-        resources = newResources;
-    }
+    // [Server]
+    // public void SetResources(int newResources)
+    // {
+    //     resources = newResources;
+    // }
 
-    #region Server
+    // #region Server
 
-    public override void OnStartServer()
-    {
-        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
-        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
-        Building.ServerOnBuildingSpawned += ServerHandleBuildingSpawned;
-        Building.ServerOnBuildingDespawned += ServerHandleBuildingDespawned;
+    // public override void OnStartServer()
+    // {
+    //     Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+    //     Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+    //     Building.ServerOnBuildingSpawned += ServerHandleBuildingSpawned;
+    //     Building.ServerOnBuildingDespawned += ServerHandleBuildingDespawned;
 
-    }
+    // }
 
-    public override void OnStopServer()
-    {
-        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
-        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
-        Building.ServerOnBuildingSpawned -= ServerHandleBuildingSpawned;
-        Building.ServerOnBuildingDespawned -= ServerHandleBuildingDespawned;
-    }
+    // public override void OnStopServer()
+    // {
+    //     Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+    //     Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+    //     Building.ServerOnBuildingSpawned -= ServerHandleBuildingSpawned;
+    //     Building.ServerOnBuildingDespawned -= ServerHandleBuildingDespawned;
+    // }
     
-    [Command]
-    public void CmdTryPlaceBuilding(int buildingID, Vector3 point)
-    {
-        Building buildingToPlace = null;
-        foreach(Building building in buildings)
-        {
-            if(building.GetId() == buildingID)
-            {
-                buildingToPlace = building;
-                break;
-            }
-        }
+    // [Command]
+    // public void CmdTryPlaceBuilding(int buildingID, Vector3 point)
+    // {
+    //     Building buildingToPlace = null;
+    //     foreach(Building building in buildings)
+    //     {
+    //         if(building.GetId() == buildingID)
+    //         {
+    //             buildingToPlace = building;
+    //             break;
+    //         }
+    //     }
 
-        if(buildingToPlace == null) {return;}
+    //     if(buildingToPlace == null) {return;}
 
-        GameObject buildingInstance = 
-            Instantiate(buildingToPlace.gameObject, point,buildingToPlace.transform.rotation);
+    //     GameObject buildingInstance = 
+    //         Instantiate(buildingToPlace.gameObject, point,buildingToPlace.transform.rotation);
 
-        NetworkServer.Spawn(buildingInstance, connectionToClient);
-    }
+    //     NetworkServer.Spawn(buildingInstance, connectionToClient);
+    // }
 
-    private void ServerHandleUnitSpawned(Unit unit)
-    {
-        if(unit.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
-        myUnits.Add(unit);
-    }
+    // private void ServerHandleUnitSpawned(Unit unit)
+    // {
+    //     if(unit.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+    //     myUnits.Add(unit);
+    // }
 
-    private void ServerHandleUnitDespawned(Unit unit)
-    {
-        if(unit.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
-        myUnits.Remove(unit);
-    }
+    // private void ServerHandleUnitDespawned(Unit unit)
+    // {
+    //     if(unit.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+    //     myUnits.Remove(unit);
+    // }
 
-    private void ServerHandleBuildingSpawned(Building building)
-    {
-        if(building.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
-        myBuildings.Add(building);
-    }
+    // private void ServerHandleBuildingSpawned(Building building)
+    // {
+    //     if(building.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+    //     myBuildings.Add(building);
+    // }
 
-    private void ServerHandleBuildingDespawned(Building building)
-    {
-        if(building.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
-        myBuildings.Remove(building);
-    }
+    // private void ServerHandleBuildingDespawned(Building building)
+    // {
+    //     if(building.connectionToClient.connectionId !=connectionToClient.connectionId) {return;}
+    //     myBuildings.Remove(building);
+    // }
 
-    #endregion
+    // #endregion
 
-    #region Client
+    // #region Client
 
-    public override void OnStartAuthority()
-    {
-        if(NetworkServer.active) {return;}
-        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
-        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
-        Building.AuthorityOnBuildingSpawned += AuthorityHandleBuildingSpawned;
-        Building.AuthorityOnBuildingDespawned += AuthorityHandleBuildingDespawned;
-    }
+    // public override void OnStartAuthority()
+    // {
+    //     if(NetworkServer.active) {return;}
+    //     Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+    //     Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+    //     Building.AuthorityOnBuildingSpawned += AuthorityHandleBuildingSpawned;
+    //     Building.AuthorityOnBuildingDespawned += AuthorityHandleBuildingDespawned;
+    // }
 
-    public override void OnStopClient()
-    {
-        if(!isClientOnly || !hasAuthority) {return;}
-        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
-        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
-        Building.AuthorityOnBuildingSpawned -= AuthorityHandleBuildingSpawned;
-        Building.AuthorityOnBuildingDespawned -= AuthorityHandleBuildingDespawned;
+    // public override void OnStopClient()
+    // {
+    //     if(!isClientOnly || !hasAuthority) {return;}
+    //     Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+    //     Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+    //     Building.AuthorityOnBuildingSpawned -= AuthorityHandleBuildingSpawned;
+    //     Building.AuthorityOnBuildingDespawned -= AuthorityHandleBuildingDespawned;
         
-    }
+    // }
 
-    private void ClientHandleResourcesUpdated(int oldVal, int newVal)
-    {
-        ClientOnResourcesUpdated?.Invoke(newVal);
-    }
+    // private void ClientHandleResourcesUpdated(int oldVal, int newVal)
+    // {
+    //     ClientOnResourcesUpdated?.Invoke(newVal);
+    // }
 
-    private void AuthorityHandleUnitSpawned(Unit unit)
-    {
-        myUnits.Add(unit);
-    }
+    // private void AuthorityHandleUnitSpawned(Unit unit)
+    // {
+    //     myUnits.Add(unit);
+    // }
 
-    private void AuthorityHandleUnitDespawned(Unit unit)
-    {
-        myUnits.Remove(unit);
-    }
+    // private void AuthorityHandleUnitDespawned(Unit unit)
+    // {
+    //     myUnits.Remove(unit);
+    // }
     
-    private void AuthorityHandleBuildingSpawned(Building building)
-    {
-        myBuildings.Add(building);
-    }
+    // private void AuthorityHandleBuildingSpawned(Building building)
+    // {
+    //     myBuildings.Add(building);
+    // }
 
-    private void AuthorityHandleBuildingDespawned(Building building)
-    {
-        myBuildings.Remove(building);
-    }
+    // private void AuthorityHandleBuildingDespawned(Building building)
+    // {
+    //     myBuildings.Remove(building);
+    // }
 
-    #endregion
+    // #endregion
 }
