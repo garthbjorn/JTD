dd924e477b0a2c1b7fdce2cc3cb4922ba3f2bf51
Garth <garthcline@gmail.com>
2022-01-01 23:00:50 -0500

    Add Steamworks and break lobbys..

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,15 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using Steamworks;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private bool useSteam = false;
 
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEnter;
+
+    private void Start()
+    {
+        if (!useSteam) { return; }
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnLobbyJoinRequested);
+        lobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEnter);
+
+    }
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            landingPagePanel.SetActive(true);
+        }
+        else
+        {
+            NetworkManager.singleton.StartHost();
+            SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAddress", SteamUser.GetSteamID().ToString());
+        }
+    }
+    private void OnLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+    private void OnLobbyEnter(LobbyEnter_t callback)
+    {
+        if(NetworkServer.active)
+        {
+            return;
+        }
+        string hostAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby),"HostAddress");
+        NetworkManager.singleton.networkAddress = hostAddress;
+        NetworkManager.singleton.StartClient();
+        landingPagePanel.SetActive(false);
+    }
     public void HostLobby()
     {
         landingPagePanel.SetActive(false);
+
+        if (useSteam)
+        {
+            // TODO: take in lobby type from the Menu and set it here.
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            return;
+        }
         NetworkManager.singleton.StartHost();
     }
 }
